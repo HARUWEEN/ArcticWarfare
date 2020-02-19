4979a48ea5ffb860da1bcfc3f118496800c16e14
xxxyouth <yungstranger@gmail.com>
2020-02-19 21:25:34 +0100

    Update Opcodes.cs
    Able to read every single packet ID/opcode.

--- ArcticEmu-master/ArcticEmu/ArctucEmu.Server/Net/Opcodes.cs
+++ ArcticEmu-master/ArcticEmu/ArctucEmu.Server/Net/Opcodes.cs
@@ -7,16 +7,24 @@ namespace ArctucEmu.Server.Net
     public enum Opcode : int
     {
 		OnConnectComplete = 0,
+		//ReqLogin = 2, //?
+        ReqCreateChar = 11,
 		ReqServerList = 13,
+		ReqChannelList = 15,
+      //ReqConnectChannelServer = 15 //also 15 wtf?
+		ReqEnterChannel = 17,
+		ReqChannelUserList = 21,
 		ReqLoginHouseTest = 25,
+		ReqEnterRoom = 27,
 		KeepAlive = 36,
 		ReqCharInfo = 38,
 		ReqErrorReportServerAddress = 56,
 		ReqClientChecksum = 54,
 		ReqClientChallenge = 52,
+		ReqConnectGame = 66,
 		ReqLoginWithGlobal = 67,
+		ReqDisconnectGame = 68,
+		ReqLogOut = 63,
 		ReqHash = OnConnectComplete,
-        ReqChannelList = 23, //not the actual number
-        ReqConnectChannelServer = 99, //not the actual number
     }
 }
